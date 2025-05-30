f1434db432c98e16c08fe5b134fa4f68e54e6a3b
Frédéric Bui <bui.frederic.pro@gmail.com>
2025-05-30 11:54:02 +0200

    feat: change api url

--- app/movies/page.js
+++ app/movies/page.js
@@ -2,13 +2,13 @@
 export default async function Page() {
 
   // Replace API url with your
-  const data = await fetch('https://dummyjson.com/products')
+  const data = await fetch('http://127.0.0.1:8000/api/movies?page=1')
   const result = await data.json()
-  const products = result.products;
+  const products = result.member;
   return (
     <ul>
       {products.map((product) => (
-        <li key={product.id}>{product.title}</li>
+        <li key={product.id}>{product.name}</li>
       ))}
     </ul>
   )
